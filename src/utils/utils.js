e50269d5702ac6e40f69b63ba149a6bf516d5990
liulihua <hc_liulihua@163.com>
2018-08-09 18:08:18 +0800

    20180809 work end

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -3,6 +3,13 @@ export default {
     formateDate(time){
         if(!time) return '';
         let date = new Date(time);
-        return date.getFullYear()+'-'+(date.getMonth()+1)+'-'+date.getDate()+' '+date.getHours()+':'+date.getMinutes()+':'+date.getSeconds();
+        let year = date.getFullYear(),
+            month = (date.getMonth()+1)>9?date.getMonth()+1:'0'+(date.getMonth()+1),
+            day = date.getDate()>9?date.getDate():'0'+date.getDate(),
+            hour = date.getHours(),
+            minutes = date.getMinutes()>9?date.getMinutes():'0'+date.getMinutes(),
+            seconds = date.getSeconds()>9?date.getSeconds():'0'+date.getSeconds();
+
+        return year+'-'+month+'-'+day+' '+hour+':'+minutes+':'+seconds;
     }
 }
