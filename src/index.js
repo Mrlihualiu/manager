f272072b8ff85ecf018d7feeaf1847e4a61a2f2e
liulihua <hc_liulihua@163.com>
2018-08-11 11:56:48 +0800

    20180811 work end

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import Admin from './admin';
+import Home from './pages/route_demo/route1/home';
 
 
-ReactDOM.render(<Admin />, document.getElementById('root'));
+ReactDOM.render(<Home />, document.getElementById('root'));
 registerServiceWorker();
--- src/pages/route_demo/route1/home.js
+++ src/pages/route_demo/route1/home.js
@@ -1,11 +1,32 @@
 import React from 'react';
-import { hashRouter, Link, Route } from 'react-router-dom';
+import { HashRouter, Link, Route } from 'react-router-dom';
+import About from './about';
+import Topics from './topics';
+import Main from './main';
 
 export default class Home extends React.Component{
 
     render(){
         return (
-             
+            <HashRouter>
+                <div>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/about">About</Link>
+                        </li>
+                        <li>
+                            <Link to="/topics">Topics</Link>
+                        </li>
+                    </ul>
+                    <hr/>
+                    <Route exact path="/" component={Main}></Route>
+                    <Route path="/about" component={About}></Route>
+                    <Route path="/topics" component={Topics}></Route>
+                </div>
+            </HashRouter>
         );
     }
 }
