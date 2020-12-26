d9404300c27df8d7206a7f4ca98b5bceef3289b4
lihua Iiu <lihuaiiu@lihuadeMacBook-Pro.local>
2020-12-26 14:32:57 +0800

    router

--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { HashRouter,Route,Switch,Redirect } from 'react-router-dom';
+
 import App from './App';
 import Admin from './admin';
 import Home from './pages/home';
